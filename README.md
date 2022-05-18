113e32b2abdfe7f608e3deb86ef9bed945c91f8a
damendozam <damendozam@unal.edu.co>
2022-05-18 09:05:14 -0500

    .

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ https://www.youtube.com/watch?v=sh-NanMOh1Q&ab_channel=Fazt
 
 ## Heroku
 https://www.youtube.com/watch?v=yNxZYN1ja4Y&t=241s&ab_channel=AndresLobaton
+
+## App
+https://pin-up-app.herokuapp.com/
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const morgan = require("morgan");
 
 //settings
-app.set('port',4000);
+app.set("port", process.env.PORT || 4000);
 app.set('views',path.join(__dirname,'ui'));
 app.engine('html',require('ejs').renderFile);
 app.set('view engine','ejs');
 app.use(express.static(__dirname + '/ui'));
 //middleware
+app.use(morgan("dev"));
 
 //routs
 app.use(require('./routes/index'));
